a2956ac2c8a170a9986f9d24224ed6ca1c3144ae
”Quin555” <QUINTIN.WEST3507@STU.MTEC.EDU>
2024-03-09 14:29:13 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const path = require("path");
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+//Starts Server
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
